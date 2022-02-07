99b124c6bcf4c7299dd41a96665d5bc285edb941
ainuyew <weiyuan.cai@gmail.com>
2022-02-07 15:48:59 +0800

    Update ze_linear_interpolation.py

--- ze_linear_interpolation.py
+++ ze_linear_interpolation.py
@@ -1,8 +1,5 @@
 import numpy as np
 from scipy import interpolate
 
-def test(ys):
+def linear_interpolate(ys):
     return ys
-    #x=[.55, .6, 65, .7, .75, .8, .85, .9, .95, 1, 1.05, 1.1, 1.15, 1.2, 1.25, 1.3, 1.35, 1.4, 1.45, 1.5, 1.7, 2]
-    #xout=np.linspace(.55, 2, 7)
-    #return [interpolate.interp1d(x, y)(xout) for y in ys]
